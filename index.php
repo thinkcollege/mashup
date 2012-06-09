a7be27c217eb654b1463e39f5fdc5571ea6c1539
paulfoos <paul.foos@umb.edu>
2012-06-09 10:11:09 -0400

    cuz

--- index.php
+++ index.php
@@ -11,10 +11,21 @@ $cleanTerm = $_REQUEST["searchTerm"] ? urlencode($searchTerm) : ""; ?>
 </head><body><h1>Library of Congress Images/New York Times Article Archive</h1><div id="searchBox"><form action="" method="get"><label for="searchTerm">Search Term: </label><input type="text" id="searchTerm" name="searchTerm" /><input type="submit" value="Submit" /></form></div><div class="clearfix"><div id="libCon"><h2>Library of Congress Image Collections</h2><?php 
 
 
-// set up request to LOC, get file	
-	$locjson = $_REQUEST["pagenext"] ? file_get_contents( 
-    'http://www.loc.gov/pictures/search/?q=' . $cleanTerm . '&fo=json&sp=' . $pagenext) : file_get_contents( 
-    'http://www.loc.gov/pictures/search/?q=' . $cleanTerm . '&fo=json');
+   $ch = curl_init();
+
+   // set the url to fetch
+ if ($_REQUEST["pagenext"]) { curl_setopt($ch, CURLOPT_URL, 'http://www.loc.gov/pictures/search/?q=' . $cleanTerm . '&fo=json&sp=' . $pagenext); } else { curl_setopt($ch, CURLOPT_URL, 'http://www.loc.gov/pictures/search/?q=' . $cleanTerm . '&fo=json'); }
+
+   // output content, not headers
+   curl_setopt($ch, CURLOPT_HEADER, 0);
+
+   // return the value instead of printing the response to browser
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+   // use a user agent to mimic a browser
+   curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.5) Gecko/20041107 Firefox/1.0');
+$locjson = curl_exec($ch); ;
+ curl_close($ch);
 // decode JSON
 $locoutput = json_decode($locjson, true);
 // pagination of LOC records
@@ -33,8 +44,26 @@ foreach($locoutput['results'] as $key => $value)
 		foreach($value['subjects'] as $subject) echo '<a href="./?searchTerm=' . $subject . '">' . $subject . '</a><br />';
 	echo '</dd></dl>';	
 	}
+	
+
+   $chii = curl_init();
+
+   // set the url to fetch
+ curl_setopt($chii, CURLOPT_URL, 'http://api.nytimes.com/svc/search/v1/article?query=title:' . $cleanTerm . '&offset=' . $nytpage . '&api-key=bfb03a1d975ff053253f4895ebd3639b:5:59463337'); 
+
+   // output content, not headers
+   curl_setopt($chii, CURLOPT_HEADER, 0);
+
+   // return the value instead of printing the response to browser
+   curl_setopt($chii, CURLOPT_RETURNTRANSFER, 1);
+
+   // use a user agent to mimic a browser
+   curl_setopt($chii, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.5) Gecko/20041107 Firefox/1.0');
+$nytjson = curl_exec($chii); ;
+ curl_close($chii);	
+	
 // get NYT data
-?></div><div id="nYt"><h2>New York Times Article Archives, 1981-2012</h2><?php $nytjson = file_get_contents( 'http://api.nytimes.com/svc/search/v1/article?query=title:' . $cleanTerm . '&offset=' . $nytpage . '&api-key=bfb03a1d975ff053253f4895ebd3639b:5:59463337');
+?></div><div id="nYt"><h2>New York Times Article Archives, 1981-2012</h2><?php
 $nytoutput = json_decode($nytjson, true); 
 
 $nytpages = ceil($nytoutput['total']/10);  
@@ -69,6 +98,9 @@ if ($currentpage != ($nytpages - 1)) {
 } 
 // end NYT pagination
 // get NYT record elements
+
+
+
 foreach($nytoutput as $key => $value) { if ($key == 'results') { foreach($value as $subkey => $subvalue) { $timestamp = strtotime($subvalue['date']);
 $makedate = date("M d Y", $timestamp);
 echo '<h3><a href="' . $subvalue['url'] . '" target="_blank">' . $subvalue['title']. '</a></h3><p class="artDate">' . $makedate . '</p>	<dl class="accordionR">
